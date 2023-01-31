ad392d03e7b73b5a902e2a113bd8092d1278b0fe
Sebastian Silva <sebastiansilvabenites@gmail.com>
2023-01-30 21:13:43 -0500

    Fix Bouncines

--- Assets/Scripts/GameManager/ScoreManager.cs
+++ Assets/Scripts/GameManager/ScoreManager.cs
@@ -1,50 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Models;
 using UnityEngine;
 
 namespace GameManager
 {
     public class ScoreManager : MonoBehaviour
     {
-        private List<int> _playerScores = new List<int> { 0, 0 };
-        
-        private TMPro.TextMeshProUGUI player1TextComponent;
-
-        private TMPro.TextMeshProUGUI player2TextComponent;
+        private List<PlayerScore> _playerScores;
 
         [SerializeField]
-        private GameObject Player1TextObject;
-
-        [SerializeField]
-        private GameObject Player2TextObject;
+        private List<GameObject> textPlayerObjects;
 
         private void Start()
         {
-            player1TextComponent = Player1TextObject.GetComponent<TMPro.TextMeshProUGUI>();
-            player2TextComponent = Player2TextObject.GetComponent<TMPro.TextMeshProUGUI>();
-
-            // TODO: refactor this so it uses a List
-            if (player1TextComponent)
+            _playerScores = new List<PlayerScore>();
+            foreach (GameObject textObject in textPlayerObjects)
             {
-                player1TextComponent.SetText(GetPlayersScoreById(0).ToString());
-            }
-            if (player2TextComponent)
-            {
-                player2TextComponent.SetText(GetPlayersScoreById(1).ToString());
+                PlayerScore newPlayerScore = new PlayerScore(textObject);
+                _playerScores.Add(newPlayerScore);
             }
         }
 
         public int GetPlayersScoreById(int playerId)
         {
             if (_playerScores.Count <= playerId) return 0;
-            return _playerScores[playerId];
+            return _playerScores[playerId].Score;
         }
 
         public void AddScoreToPlayer(int playerId, int scoreToAdd = 1)
         {
             if (_playerScores.Count <= playerId) return;
-            _playerScores[playerId] += scoreToAdd;
-            player1TextComponent.SetText(_playerScores[playerId].ToString());
+            _playerScores[playerId].AddScore(scoreToAdd);
         }
     }
 
---
+++ Assets/Scripts/Models/PlayerScore.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Models
+{
+    public class PlayerScore
+    {
+        public int Score { get; set; }
+
+        public GameObject TextObject;
+
+        private TMPro.TextMeshProUGUI _textComponent;
+
+        public PlayerScore(GameObject textObject)
+        {
+            TextObject = textObject;
+            Score = 0;
+            _textComponent = TextObject.GetComponent<TMPro.TextMeshProUGUI>();
+        }
+
+        public void AddScore(int scoreToAdd = 1)
+        {
+            if (_textComponent == null) return;
+            Score += scoreToAdd;
+            _textComponent.SetText(Score.ToString());
+        }
+    }
+}
